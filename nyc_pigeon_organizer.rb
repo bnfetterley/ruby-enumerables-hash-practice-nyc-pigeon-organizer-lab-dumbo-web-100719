2e51c19ecb604ad0d2d52119e3a456e00c8fd94a
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-30 16:59:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,27 +1,27 @@
 require 'pry'
 
-  def nyc_pigeon_organizer(data)
-  new_hash = {}
-  data.each do |key, value|
-    value.each do |new_value, names|
-      names.each do |name|
+#   def nyc_pigeon_organizer(data)
+#   new_hash = {}
+#   data.each do |key, value|
+#     value.each do |new_value, names|
+#       names.each do |name|
        
-        if !new_hash[name]
-          new_hash[name] = {}
-        end
+#         if !new_hash[name]
+#           new_hash[name] = {}
+#         end
         
-        if !new_hash[name][key]
-          new_hash[name][key] = []
-         new_hash[name][key] << new_value.to_s 
-      # binding.pry
-        end
+#         if !new_hash[name][key]
+#           new_hash[name][key] = []
+#         new_hash[name][key] << new_value.to_s 
+#       # binding.pry
+#         end
         
-        end
-      end
-    end
-  new_hash
+#         end
+#       end
+#     end
+#   new_hash
   
-end
+# end
 
 
 
