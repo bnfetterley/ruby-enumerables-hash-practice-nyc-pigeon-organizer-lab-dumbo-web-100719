ae17fdd02fdf862ad2d6e011538f01207adf94cf
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-30 16:09:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,6 @@ def nyc_pigeon_organizer(data)
       names.each do |name|
        if !new_hash[name]
         new_hash[name] = {}
-        if  new_hash[name] 
           new_hash[name][key] = []
       end
       end
